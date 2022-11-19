89277ced941d16b8471b7743542c2fe945c258b9
Abdul Aleem <ma.aleem20@gmail.com>
2022-11-19 17:12:56 +0400

    new

--- print.sh
+++ print.sh
@@ -1,10 +1,5 @@
-echo "Hello World"
 a=10
 echo $a
-echo "this is the first step to success"
-b=(40 50 60)
-echo "${b[2]}"
-c=(apple banana oranges)
-d=(paltop computer)
-echo "${c[0]}"
-echo "${d[0,1]}"
+b=20
+echo $b
+readonly b
